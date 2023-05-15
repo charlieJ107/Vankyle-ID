939f5d0aa9f161a703f0c7edd137c13dc2d5d7c9
charlieJ107 <42380833+charlieJ107@users.noreply.github.com>
2023-05-15 14:05:00 +0100

    Add docker support

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Vankyle ID greeting', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/It's all here with Vankyle ID./);
   expect(linkElement).toBeInTheDocument();
 });
