094fdafc550755eb80309d830e6a8a6632860f99
charlieJ107 <charlie_j107@outlook.com>
2024-11-07 00:28:38 +0000

    Add login page with MVC and thymeleaf

---
+++ src/main/java/com/vankyle/id/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.vankyle.id.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
